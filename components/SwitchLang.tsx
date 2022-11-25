f4791f2ed6b4e473817ee5c9cef6d14592943204
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2022-11-25 17:54:55 +0700

    fix case in language switch function

--- components/SwitchLang.tsx
+++ components/SwitchLang.tsx
@@ -19,16 +19,10 @@ const localeText = (locale: string): string => {
   switch (locale) {
     case 'en':
       return '🇬🇧 English'
-    case 'zh-CN':
-      return '🇨🇳 简体中文'
-    case 'hi':
-      return '🇮🇳 हिन्दी'
-    case 'tr-TR':
-      return '🇹🇷 Türkçe'
-    case 'zh-TW':
-      return '🇹🇼 繁體中文'
+    case 'vi-VN':
+      return 'vɴ Tiếng Việt'
     default:
-      return '🇬🇧 English'
+      return 'vɴ Tiếng Việt'
   }
 }
 
