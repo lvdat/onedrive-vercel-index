73c72225939d0ba88be44543629076ab05451af3
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2022-11-25 16:36:06 +0700

    remove hi, TR, CN, TW language, add Vietnamese language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh-CN', 'hi', 'tr-TR', 'zh-TW']
+    defaultLocale: 'vi-VN',
+    locales: ['en', 'vi-VN']
   },
   localePath: path.resolve('public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
