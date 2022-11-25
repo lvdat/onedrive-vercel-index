244a2c5e1fcee4da83bfb57fc779f1a811e2221f
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2022-11-25 18:38:36 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "levandat Get Link OneDrive",
+  title: "Get Link OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Web',
