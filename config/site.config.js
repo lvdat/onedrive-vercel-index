bf21b32bbd924a54164e76be0177cf67e4f73d32
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2023-02-06 15:41:02 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,10 +17,10 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Get Link OneDrive",
+  title: "CICT IT Club Drive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/images.ctuit.club',
+  baseDirectory: '/CICTWeb',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
